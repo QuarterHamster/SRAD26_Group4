74ee43df339da0b8335f75c7135352784d83b027
Elmar-Sig <elmar.sigmars@gmail.com>
2026-03-06 13:55:09 +0000

    In AdminLogic and admin event review, added a retrun that is a message of the decision from the admin

--- Project/LogicLayer/AdminLogic.py
+++ Project/LogicLayer/AdminLogic.py
@@ -6,12 +6,15 @@ class AdminLogic():
     def admin_event_review(self, event: Event, decision: bool):
         """
         Takes in an event Object and the decision of the admin (True/False)
+
+        Returns a message from the decision
         """
         
         # If the decision is True
         if decision:
             event.status = Event_status.ACTIVE
+            return "Event has been approved"
         
         # If the decision is False
-        else: del(event)
+        else: return "Event has been declined"
 
