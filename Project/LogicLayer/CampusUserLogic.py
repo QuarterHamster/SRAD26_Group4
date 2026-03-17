c6dad5fa0786efc0ec2d6d151bb0dc15ceebcd03
Elmar-Sig <elmar.sigmars@gmail.com>
2026-03-17 15:21:20 +0000

    created a CampusUserLogic file and implemented view old events, and added it to the LogicLayerAPI

---
+++ Project/LogicLayer/CampusUserLogic.py
@@ -0,0 +1,17 @@
+from UILayer.Place_holder_data import campus_users, events
+from Models.Campus_user import Campus_user
+from Models.Enums import Event_status
+
+class CampusUserLogic:
+    def view_old_events(self, user: Campus_user)-> list[str]:
+        """
+        Looks through an list of events and returns a list of event names that the user has attended
+        """
+        past_events: list = []
+
+        for i in events:
+            if (i.status is Event_status.ENDED) and (user.name in i.attendees) :
+                past_events.append(i.event_name)
+        
+        if not past_events: return "You have no past events"
+        return past_events
