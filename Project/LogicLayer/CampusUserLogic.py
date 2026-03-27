611434c5738e103a07729cf2544437dbd81b9d7d
sindri <sindrif25@ru.is>
2026-03-27 12:06:46 +0000

    feat(favorite): Created favorite view function in CampusUserLogic.py

--- Project/LogicLayer/CampusUserLogic.py
+++ Project/LogicLayer/CampusUserLogic.py
@@ -17,3 +17,24 @@ class CampusUserLogic:
         if not past_events:
             return []
         return past_events
+
+    def view_favorite_events(self, user: Campus_user) -> list[str]:
+        """
+        Looks through a list of events and returns a list of event names that the user has favorite
+        :param user: Campus_user object
+        :return: List of event names that the user has favorited
+        """
+        favorite_events: list = []
+        for i in events:
+            if i.event_name in user.favorites:
+                favorite_events.append(i.event_name)
+
+        if not favorite_events:
+            return []
+        return favorite_events
+
+    def favorite_event(self, user: Campus_user, event: str):
+        return user.add_favorite_event(event)
+
+    def unfavorite_event(self, user: Campus_user, event: str):
+        return user.remove_favorite_event(event)
